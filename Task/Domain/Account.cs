9cc12c0d09ca522e5dadda9985b6133c3ed1a750
Myroslav <muroslavkl@gmail.com>
2024-09-12 17:35:10 +0300

    feature: filled domain project with classes

---
+++ Task/Domain/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Base;
+
+namespace Domain;
+
+public class Account:BaseEntity
+{
+    [Required] [StringLength(50)] 
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
+    public ICollection<Incident> Incidents { get; set; } = new List<Incident>();
+}
