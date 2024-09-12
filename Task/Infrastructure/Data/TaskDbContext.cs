e23f3da7376ac8213570ef76b5322bbe81f3c432
Myroslav <muroslavkl@gmail.com>
2024-09-12 17:49:11 +0300

    feature: added Context

---
+++ Task/Infrastructure/Data/TaskDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class TaskDbContext:DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Incident> Incidents { get; set; }
+
+    public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>()
+            .HasIndex(a => a.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<Contact>()
+            .HasIndex(c => c.Email)
+            .IsUnique();
+        
+    }
+}
