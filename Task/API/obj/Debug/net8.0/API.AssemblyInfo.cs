90033180921db7e0a3ed29536e4e209c19c112dc
Myroslav <muroslavkl@gmail.com>
2024-09-14 17:15:17 +0300

    feature:added controller with seeding test data

--- Task/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Task/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88045e1c1150dfe9c38f597700c9e028a8922c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38465813de88bfb4f607e9a4db2f142c175467b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task/Infrastructure/Migrations/20240914140326_SeedDataToTest.Designer.cs
+++ Task/Infrastructure/Migrations/20240914140326_SeedDataToTest.Designer.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(TaskDbContext))]
-    partial class TaskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240914140326_SeedDataToTest")]
+    partial class SeedDataToTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
